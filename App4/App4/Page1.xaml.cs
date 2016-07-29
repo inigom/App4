267250202bb7996799ae8a690bf150f6e812b1e5
ini_m <ini_m@INI>
2016-07-29 13:01:31 +0200

    Agregar archivos de proyecto.

---
+++ App4/App4/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App4
+{
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+        void PulsadoClick(object o, EventArgs e)
+        {
+    
+            salida.Text = entrada.Text;
+        }
+        async void Navegara2(object o , EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2(entrada.Text)); 
+        }
+        
+
+    }
+}
